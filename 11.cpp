0e5044488d06d5a9848a378df8113d1806e2280c
problem solving <abdelrhman78p@gmail.com>
2025-01-29 02:30:56 +0200

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-string substring(const string& st,int start,int end,bool inc_end=true,bool count_space = false){
+// code didn't include reverse or step till watched the video
+string substring(const string& st, const int& start, int end, const int& step = 1, bool inc_end=true,bool count_space = false, bool rev = false){
 
 
 string res;
+string temp;
 
-for (int j = start; inc_end? j<=end : j<end; ++j) {
+for (int j = start; inc_end? j<=end : j<end; j += step) {
     
     if (!count_space&&st[j]==' '){++end;}
+    if (rev)
+    {
+        res = st[j] + res;
+        continue;
+    }
     
 
      res += st[j]; }
+
+// if (rev) {
+// for (int i = res.length()-1;; --i)
+// {
+//      temp += res[i];
+// if(i == 0){return temp;}
+// }
+// }
+
 
 
 return res;
@@ -20,7 +35,7 @@ return res;
 }
 int main(){
 
-   cout << substring("zydom web school",0,9,true,true); //اوعى الكرييتف ابداع في اختيار ال سترينج
+   cout << substring("zydom web school",0,9,1,true,true,true); //اوعى الكرييتف ابداع في اختيار ال سترينج
    // substring();
    // substring();
 

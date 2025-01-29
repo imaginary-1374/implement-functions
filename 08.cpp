789c2a486e844d0e9fc586ff5db3738440ed98ae
problem solving <abdelrhman78p@gmail.com>
2025-01-29 03:08:41 +0200

    Update 08.cpp

--- 08.cpp
+++ 08.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 using namespace std;
 
-string parse_integer(const string& st ){
-string digits;
+void parse_integer(const string& st ){
+char ascii;
  if (st.empty()) {cout << "ERROR 404 : st\nstring not found\n";
- return digits;
+ return;
  }
-  for (char c : st) {
-   if(c >= '0' && c <= '9'){ digits += c;}
-    }
+  for (char c : st){ 
+    
+    if(c >= '0' && c <= '9'){
+    ascii = c;
+    cout << int(ascii) << endl;
+    }}
 
-    return digits; 
 }
 
 int main(){
 
- cout << parse_integer("z2ydo3sad1wad5zxc09m\n111");
+parse_integer("1a2^3@#ks!#e5893&%634\n ij2");
+
 return 0;
 }
 
@@ -23,6 +26,7 @@ return 0;
 // حل الزيرو بيحول البيانات من سترينج ل انتجر
 // digits = digits *10 + (c -'0');
 //كود الزيرو
+// تم تعديل الكود 
 
 /*
 int parse_integer(const string& st ){
@@ -39,12 +43,9 @@ int digits = 0;
 */
 /*
 |Aspect|	            |Your Code|	                                | Mentor's Code|
-|Output Type	        |Returns a string of digits.	            |Returns an integer value.
-|Memory Efficiency	    |Uses more memory (to store a string).	    |More memory-efficient (stores a single int).
-|Versatility	        |Easier to extend for non-integer parsing.	|Specific to numeric parsing.
-|Beginner Friendliness	|Clearer and easier to understand initially.|Slightly more complex due to math operations.
-|Use Case	            |Suitable if you need the digits as text.	|Suitable if you need the number directly.
-
-SUMMARY: 
-الزيرو أجمد
+|Output Type	        | Returns a string of digits.	            | Returns an integer value.
+|Memory Efficiency	    | Uses more memory (to store a string).	    | More memory-efficient (stores a single int).
+|Versatility	        | Easier to extend for non-integer parsing.	| Specific to numeric parsing.
+|Beginner Friendliness	| Clearer and easier to understand initially.| Slightly more complex due to math operations.
+|Use Case	            | Suitable if you need the digits as text.	| Suitable if you need the number directly.
 */
